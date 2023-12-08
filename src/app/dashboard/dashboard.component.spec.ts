2babbf7490f4e38ccfe20b4ae9d6a88df8887726
CSVerbeek <csverbeek89@gmail.com>
2023-12-08 16:04:15 +0100

    Create task list component and pass dashboard test

--- src/app/dashboard/dashboard.component.spec.ts
+++ src/app/dashboard/dashboard.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DashboardComponent } from './dashboard.component';
+import { TaskListComponent } from './task-list/task-list.component';
 
 describe('DashboardComponent', () => {
     let component: DashboardComponent;
@@ -8,7 +9,7 @@ describe('DashboardComponent', () => {
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            declarations: [DashboardComponent]
+            declarations: [DashboardComponent, TaskListComponent],
         })
             .compileComponents();
 
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
+import { TaskListComponent } from './task-list/task-list.component';
 
 
 @NgModule({
     declarations: [
-        DashboardComponent
+        DashboardComponent,
+        TaskListComponent
     ],
     imports: [
         CommonModule,
