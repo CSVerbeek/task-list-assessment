4204ecf97a10ef0cb6731e2a5eef3ab62ac77e32
CSVerbeek <csverbeek89@gmail.com>
2023-12-10 13:07:34 +0100

    Add routing from task list to tasks details

--- src/app/dashboard/task-list/task-list.component.ts
+++ src/app/dashboard/task-list/task-list.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Task } from '../../shared/task';
 import { ITaskService, TASK_SERVICE } from '../../i-task.service';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'tla-task-list',
@@ -11,7 +12,11 @@ import { ITaskService, TASK_SERVICE } from '../../i-task.service';
 export class TaskListComponent {
     readonly tasks$: Observable<Task[]>;
 
-    constructor(@Inject(TASK_SERVICE) public taskService: ITaskService) {
+    constructor(@Inject(TASK_SERVICE) public taskService: ITaskService, private router: Router) {
         this.tasks$ = this.taskService.tasks$;
+    }
+
+    onTaskRowClick(id: Task['id']): void {
+        this.router.navigate(['/task-details', id]);
     }
 }
