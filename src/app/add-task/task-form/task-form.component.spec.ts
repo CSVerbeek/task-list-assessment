f6fe1f88bd6a6a7ff6402f317d55f5097aa19b12
CSVerbeek <csverbeek89@gmail.com>
2023-12-10 14:10:07 +0100

    Add test for rendered form fields

--- src/app/add-task/task-form/task-form.component.spec.ts
+++ src/app/add-task/task-form/task-form.component.spec.ts
@@ -2,22 +2,32 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TaskFormComponent } from './task-form.component';
 
-describe('TaskFormComponent', () => {
-  let component: TaskFormComponent;
-  let fixture: ComponentFixture<TaskFormComponent>;
+fdescribe('TaskFormComponent', () => {
+    let component: TaskFormComponent;
+    let fixture: ComponentFixture<TaskFormComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [TaskFormComponent]
-    })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(TaskFormComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [TaskFormComponent]
+        })
+            .compileComponents();
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        fixture = TestBed.createComponent(TaskFormComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should have form fields for title, description and status', () => {
+        const nativeElement: HTMLElement = fixture.nativeElement;
+        const titleFormField: HTMLElement | null = nativeElement.querySelector('input.task-title');
+        const descriptionFieldElement: HTMLElement | null = nativeElement.querySelector('input.task-description');
+        const statusFieldElement: HTMLElement | null = nativeElement.querySelector('mat-select.task-description');
+        expect(titleFormField).withContext('title form field is rendered').not.toBeNull();
+        expect(descriptionFieldElement).withContext('description form field is rendered').not.toBeNull();
+        expect(statusFieldElement).withContext('status form field is rendered').not.toBeNull();
+    });
 });
